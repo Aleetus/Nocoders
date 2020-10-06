4f4282ec90d3181c35711db12cd1858dfb6ac91e
Michael Wiggins <u1807402@uel.ac.uk>
2020-10-06 16:21:04 +0100

    Basic Function Menu
    Start will load specified scene (input in inspector) on click
    
    Credits will load 'Credits' scene on click
    
    Exit on click will close the .exe
    
    Mute button will mute/unmute audio  on click and change sprite to indicate toggled mode

---
+++ Nocoders_Transmission_Project/Assets/Scripts/TimedDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 140);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
