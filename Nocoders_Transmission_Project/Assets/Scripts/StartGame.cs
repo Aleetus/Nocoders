909545eabbeb4b5cb6b02e1de69d8c1c7ffaa439
Michael Wiggins <u1807402@uel.ac.uk>
2020-10-09 22:44:39 +0100

    added bits from Kory
    .

---
+++ Nocoders_Transmission_Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    public float timeLeft = 3.0f;
+    public Text startText;
+    public Text playerCount;
+    
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        startText.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            
+        }
+        if (GameObject.FindGameObjectsWithTag("uninfected").Length > 1)  
+        {
+            playerCount.text = GameObject.FindGameObjectsWithTag("uninfected").Length.ToString();
+
+        }
+    }
+}
